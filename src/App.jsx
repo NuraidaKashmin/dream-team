9adecb67f84e3b717add8e4119f20f7abf5910a6
NuraidaKashmin <nuraidakkashmin@gmail.com>
2024-10-24 22:46:14 +0600

    initial installation done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Dream Team React</h1>
+      
+
+    </>
+  )
+}
+
+export default App
